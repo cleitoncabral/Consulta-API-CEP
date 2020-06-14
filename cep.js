e3e9f04d5db15c05c1ff95aeb755afe344eb9e24
Cleiton Cabral <55886803+cleitoncabral@users.noreply.github.com>
2020-06-14 14:38:10 -0300

    Buscando e exibindo

---
+++ cep.js
@@ -0,0 +1,18 @@
+const cep = document.querySelector('#cep')
+
+cep.addEventListener('blur',(e) =>{
+    let search = cep.value.replace('-','')
+
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then( response => {response.json()
+        .then( data => console.log(data))
+    })
+    .catch( erro => console.error(erro))
+    
+} )
